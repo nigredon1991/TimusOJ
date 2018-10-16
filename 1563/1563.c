2d3b50ffdcda84b7f1987b139d2c5933e66bb98b
nigredon1991 <nigredon1991@gmail.com>
2018-10-16 23:48:16 +0500

    1009 and 1081

---
+++ 1563/1563.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int hash_map(const char *str)
+{
+	unsigned long hash = 6489;
+	int c;
+
+	while (c = *str++) {
+		if (c == 0)
+			break;
+		hash = ((hash << 4) + hash) + c; /* hash * 33 + c */
+	}
+
+	return (hash % 100000);
+}
+
+int hash_map2(const char *str)
+{
+	unsigned long hash = 5381;
+	int c;
+
+	while (c = *str++) {
+		if (c == 0)
+			break;
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+
+	return (hash % 100000);
+}
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	int hash[100000];
+	int hash2[100000];
+	char buffer[50];
+	int calc = 0;
+	for (int i = 0; i <= N; i++) {
+		gets(buffer);
+		scanf("^%s$", &buffer);
+		/* printf("%s\n", buffer); */
+		if (hash[hash_map(buffer)] == 'B' &&
+		    hash2[hash_map2(buffer)] == 'B')
+			calc++;
+		else
+			hash[hash_map(buffer)] = 'B';
+		hash2[hash_map2(buffer)] = 'B';
+		/* printf("%d\n", hash_map(buffer)); */
+	}
+	printf("%d\n", calc);
+
+	return 0;
+}
