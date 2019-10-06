dd25352894d87f48047403c9d7be8bbe6e84fbda
nigredon1991 <nigredon1991@gmail.com>
2019-10-06 09:33:41 +0500

    Add 1.1.14

---
+++ Shen/1.1.14/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+	int a, b, k, m, n;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	m = a;
+	n = b;
+
+	while (!((n == 0) || (m == 0))) {
+		if (m >= n) {
+			m = m % n;
+		} else {
+			n = n % m;
+		}
+	}
+	if (m == 0) {
+		k = n;
+	} else {
+		k = m;
+	}
+
+	printf("%d\n", k);
+	return 0;
+}
