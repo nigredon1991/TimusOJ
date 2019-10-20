3b4aaaebf50672097cb89c0b530e6c7d77984ed1
nigredon1991 <nigredon1991@gmail.com>
2019-10-20 18:36:18 +0500

    Add 1.1.15

---
+++ Shen/1.1.15/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+	int a, b, k, m, n, p, q, r, s, x, y;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	m = a;
+	n = b;
+	p = 1;
+	q = 0;
+	r = 0;
+	s = 1;
+	while (!((n == 0) || (m == 0))) {
+		if (m >= n) {
+			m = m - n;
+			p = p - r;
+			q = q - s;
+		} else {
+			n = n - m;
+			r = r - q;
+			s = s - q;
+		}
+	}
+	if (m == 0) {
+		k = n;
+		x = r;
+		y = s;
+	} else {
+		k = m;
+		x = p;
+		y = q;
+	}
+
+	printf("%d %d %d\n", k, x, y);
+	return 0;
+}
