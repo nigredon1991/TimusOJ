0ad24ffa1860d54cb802ae5b0b0f267009971754
nigredon1991 <nigredon1991@gmail.com>
2020-08-06 22:04:51 +0500

    Add 1.1.28

--- Shen/1.1.29/main.c
+++ Shen/1.1.29/main.c
@@ -4,7 +4,6 @@
 int count_decision(int k2, int n) {
 	int l = 0, t = 0;
 	while (k2 + l * l < n) {
-		/* printf("k2: %d, l: %d, t:%d\n", k2, l, t); */
 		l = l + 1;
 		t = t + 1;
 	}
@@ -12,14 +11,18 @@ int count_decision(int k2, int n) {
 }
 
 int main() {
-	int n, k = 0, s = 0, t;
+	int n, k = 0, l = 0, s = 0;
 	scanf("%d", &n);
-	while (k * k < n) {
-		t = count_decision(k * k, n);
-		k = k + 1;
-		s = s + t;
+	while (l * l < n) {
+		l = l + 1;
 	}
-
+	while (l != 0) {
+		s += l;
+		k++;
+		while (l != 0 && k * k + (l - 1) * (l - 1) >= n) {
+			l--;
+		}
+	}
 	printf("%d\n", s);
 	return 0;
 }
