56a7b1173bfe9e1dd145d0b4aff6fa49347d77c8
nigredon1991 <nigredon1991@gmail.com>
2022-06-30 20:15:00 +0500

    fix

--- Shen/1.1.31/main.c
+++ Shen/1.1.31/main.c
@@ -1,22 +1,15 @@
 #include <assert.h>
 #include <stdio.h>
 
-// TODO Надо написать тесты, возможно поправить
 int main() {
-  int n, l = 0, r = 1;
-  scanf("%d", &n);
-  while (l != n + 1) {
+  int n, k, l = 0, r = 1;
+  scanf("%d %d", &n, &k);
+  while (l != k) {
+    printf("%d", 10 * r / n);
     r = (10 * r) % n;
     l = l + 1;
   }
-  int c = r;
-  r = (10 * r) % n;
-  int k = 1;
-  while (r != c) {
-    r = (10 * r) % n;
-    k = k + 1;
-  }
-  printf("%d\n", r);
+  printf("\n");
 
   return 0;
 }
