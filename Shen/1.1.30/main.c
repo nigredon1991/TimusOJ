9298f5d1776a02ef87c4e86a4aa7b4e05bd36dd4
nigredon1991 <nigredon1991@gmail.com>
2022-07-03 12:02:39 +0500

    Fix tests

--- Shen/1.1.30/main.c
+++ Shen/1.1.30/main.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 int main() {
-//TODO Не помню, что тут вообще происходит, надо разобраться
-	int n, k, l = 0, r = 1;
-	scanf("%d %d", &n, &k);
-	while (l != k) {
-		printf("%d", 10 * r / n);
-		r = 10 * r % n;
-		l = l + 1;
-	}
+  int n, k, l = 0, r = 1;
+  scanf("%d %d", &n, &k);
+  while (l != k) {
+    printf("%d", 10 * r / n);
+    r = (10 * r) % n;
+    l = l + 1;
+  }
+  printf("\n");
 
-	return 0;
+  return 0;
 }
